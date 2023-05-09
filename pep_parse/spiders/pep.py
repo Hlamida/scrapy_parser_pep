f7fd2544701f527c2001cfe6d5b3acbb971c1ca2
Hlamida <108410042+Hlamida@users.noreply.github.com>
2023-05-09 09:50:07 +0300

    2 results

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,33 @@
+import re
+import scrapy
+
+from urllib.parse import urljoin
+
+from pep_parse.constants import MAIN_PEPS_URL
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['http://peps.python.org/']
+
+    def parse_pep(self, response):
+        page_title = response.css('h1.page-title::text').get()
+        parsed_number = int(re.findall(r'\d+', page_title)[0])
+        parsed_name = re.sub(r'PEP \d+ \S', ' ', page_title).strip()
+        parsed_status = response.css('abbr::text').get()
+        data = {
+            'number': parsed_number,
+            'name': parsed_name,
+            'status': parsed_status,
+        }
+        yield PepParseItem(data)
+
+    def parse(self, response):
+        all_peps = response.css(
+            '#numerical-index a[href^="pep"]::attr(href)'
+            ).getall()
+        for short_link in all_peps:
+            pep_link = urljoin(MAIN_PEPS_URL, short_link)
+            yield response.follow(pep_link, callback=self.parse_pep)
